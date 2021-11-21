e0c2e6e4f03c0ff6f8b36b0c127b7b34068308f8
lophk <61011212114@msu.ac.th>
2021-11-21 11:27:47 +0700

    link component

--- animesh-front/src/app/app-routing.module.ts
+++ animesh-front/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AnimeshHomePageComponent } from './animesh-home-page/animesh-home-page.component';
+const routes: Routes = [
+  {path:'',component:AnimeshHomePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

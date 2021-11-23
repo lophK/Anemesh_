699396271e2ece1d419832d066034c242eee705b
61011212073 <61011212073@msu.ac.th>
2021-11-23 17:51:46 +0700

    Tle

--- animesh-front/src/app/app.module.ts
+++ animesh-front/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimeshHomePageComponent } from './animesh-home-page/animesh-home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnimeshHomePageComponent
+    AnimeshHomePageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    IvyCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
